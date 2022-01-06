542f2e785abd0eb1f3097d1c6c89e7551f4b532d
Sarah Bass <69780815+SarahBass@users.noreply.github.com>
2022-01-06 03:01:39 -0800

    Add files via upload

---
+++ CAR_zeldawhistle.ino
@@ -0,0 +1,260 @@
+/*
+* @par Copyright (C): 2010-2019, Shenzhen Yahboom Tech
+* @file         Car_whistle.c
+* @author       Cindy
+* @version      V1.0
+* @date         2018.10.19
+* @brief        
+* @details
+* @par History  
+*
+*/
+#include "RGBLed.h"
+#include <Arduino.h>                 //Library file
+#include "./Adafruit_NeoPixel.h"     //Library file
+#define PIN 6                       //Define the pins of the RGB light
+#define MAX_LED 1                   //Just one RGB light on the car
+Adafruit_NeoPixel strip = Adafruit_NeoPixel( MAX_LED, PIN, NEO_RGB + NEO_KHZ800 );
+uint8_t i = 0;                            
+uint32_t redbright = strip.Color(0,255,0);   //green,red,blue
+uint32_t bluebright = strip.Color(0,0,255);   //green,red,blue
+uint32_t greenbright = strip.Color(255,0,0);   //green,red,blue
+uint32_t red = strip.Color(0,1,0);   //green,red,blue
+uint32_t blue = strip.Color(0,0,1);   //green,red,blue
+uint32_t green = strip.Color(1,0,0);   //green,red,blue
+uint32_t greenblue = strip.Color(1,0,1);   //green,red,blue
+uint32_t skyblue = strip.Color(10,0,10);   //green,red,blue
+uint32_t teal = strip.Color(10,0,5);   //green,red,blue
+uint32_t black = strip.Color(0,0,0);   //green,red,blue
+uint32_t purple = strip.Color(0,1,1);   //green,red,blue
+uint32_t bluepurple = strip.Color(0,1,3);   //green,red,blue
+uint32_t magenta = strip.Color(0,20,5);   //green,red,blue
+uint32_t toohotpink = strip.Color(0,10,1);   //green,red,blue
+uint32_t pink = strip.Color(0,5,1);   //green,red,blue
+uint32_t orange = strip.Color(1,10,0);   //green,red,blue
+uint32_t brightwhite = strip.Color(255,255,255);   //green,red,blue
+uint32_t white = strip.Color(100,100,100);   //green,red,blue
+uint32_t dullwhite = strip.Color(10,10,10);   //green,red,blue
+uint32_t sortofwhite = strip.Color(1,1,1);   //green,red,blue
+#define RGB_GREEN    0xFF0000    //Define different color(green,red,blue)
+#define RGB_RED   0x00FF00
+#define RGB_BLUE    0x0000FF
+#define RGB_YELLOW  0xFFFF00
+#define RGB_PURPLE  0x00FFFF
+#define RGB_WHITE   0xFFFFFF
+#define RGB_OFF  0x000000
+const int RgbPin = 11;    //Define pin of Ultrasonic RGB light
+RGBLed mRgb(RgbPin,2);
+
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+#define REST 0
+
+// change this to make the song slower or faster
+int tempo = 150;
+
+// change this to whichever pin you want to use
+int buzzer = 10;
+
+// notes of the moledy followed by the duration.
+// a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
+// !!negative numbers are used to represent dotted notes,
+// so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
+int melody[] = {
+
+  //Based on the arrangement at https://www.flutetunes.com/tunes.php?id=169
+  
+  NOTE_AS4,-2,  NOTE_F4,8,  NOTE_F4,8,  NOTE_AS4,8,//1
+  NOTE_GS4,16,  NOTE_FS4,16,  NOTE_GS4,-2,
+  NOTE_AS4,-2,  NOTE_FS4,8,  NOTE_FS4,8,  NOTE_AS4,8,
+  NOTE_A4,16,  NOTE_G4,16,  NOTE_A4,-2,
+  REST,1, 
+
+  NOTE_AS4,4,  NOTE_F4,-4,  NOTE_AS4,8,  NOTE_AS4,16,  NOTE_C5,16, NOTE_D5,16, NOTE_DS5,16,//7
+  NOTE_F5,2,  NOTE_F5,8,  NOTE_F5,8,  NOTE_F5,8,  NOTE_FS5,16, NOTE_GS5,16,
+  NOTE_AS5,-2,  NOTE_AS5,8,  NOTE_AS5,8,  NOTE_GS5,8,  NOTE_FS5,16,
+  NOTE_GS5,-8,  NOTE_FS5,16,  NOTE_F5,2,  NOTE_F5,4, 
+
+  NOTE_DS5,-8, NOTE_F5,16, NOTE_FS5,2, NOTE_F5,8, NOTE_DS5,8, //11
+  NOTE_CS5,-8, NOTE_DS5,16, NOTE_F5,2, NOTE_DS5,8, NOTE_CS5,8,
+  NOTE_C5,-8, NOTE_D5,16, NOTE_E5,2, NOTE_G5,8, 
+  NOTE_F5,16, NOTE_F4,16, NOTE_F4,16, NOTE_F4,16,NOTE_F4,16,NOTE_F4,16,NOTE_F4,16,NOTE_F4,16,NOTE_F4,8, NOTE_F4,16,NOTE_F4,8,
+
+  NOTE_AS4,4,  NOTE_F4,-4,  NOTE_AS4,8,  NOTE_AS4,16,  NOTE_C5,16, NOTE_D5,16, NOTE_DS5,16,//15
+  NOTE_F5,2,  NOTE_F5,8,  NOTE_F5,8,  NOTE_F5,8,  NOTE_FS5,16, NOTE_GS5,16,
+  NOTE_AS5,-2, NOTE_CS6,4,
+  NOTE_C6,4, NOTE_A5,2, NOTE_F5,4,
+  NOTE_FS5,-2, NOTE_AS5,4,
+  NOTE_A5,4, NOTE_F5,2, NOTE_F5,4,
+
+  NOTE_FS5,-2, NOTE_AS5,4,
+  NOTE_A5,4, NOTE_F5,2, NOTE_D5,4,
+  NOTE_DS5,-2, NOTE_FS5,4,
+  NOTE_F5,4, NOTE_CS5,2, NOTE_AS4,4,
+  NOTE_C5,-8, NOTE_D5,16, NOTE_E5,2, NOTE_G5,8, 
+  NOTE_F5,16, NOTE_F4,16, NOTE_F4,16, NOTE_F4,16,NOTE_F4,16,NOTE_F4,16,NOTE_F4,16,NOTE_F4,16,NOTE_F4,8, NOTE_F4,16,NOTE_F4,8
+  
+};
+
+// sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
+// there are two values per note (pitch and duration), so for each note there are four bytes
+int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+
+// this calculates the duration of a whole note in ms (60s/tempo)*4 beats
+int wholenote = (60000 * 4) / tempo;
+
+int divider = 0, noteDuration = 0;
+/**
+* Function       setup
+* @author        Cindy
+* @date          2019.07.29
+* @brief         Initial configuration
+* @param[in]     void
+* @retval        void
+* @par History   no
+*/
+void setup() {
+   strip.begin();           
+  strip.show();
+  strip.setPixelColor(i, blue);  //Make Onboard RGB light
+      strip.show();
+       mRgb.setColor(1,RGB_OFF);
+    mRgb.setColor(2,RGB_OFF);
+    mRgb.show();
+       delay(500);   
+  // iterate over the notes of the melody. 
+  // Remember, the array is twice the number of notes (notes + durations)
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+
+    // calculates the duration of each note
+    divider = melody[thisNote + 1];
+    if (divider > 0) {
+          mRgb.setColor(1,RGB_GREEN);
+          mRgb.setColor(2,RGB_GREEN);
+          mRgb.show();
+      // regular note, just proceed
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      // dotted notes are represented with negative durations!!
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5; // increases the duration in half for dotted notes
+          mRgb.setColor(1,RGB_YELLOW);
+          mRgb.setColor(2,RGB_YELLOW);
+          mRgb.show();
+    }
+    
+    // we only play the note for 90% of the duration, leaving 10% as a pause
+    tone(buzzer, melody[thisNote], noteDuration*0.9);
+    
+    // Wait for the specief duration before playing the next note.
+    delay(noteDuration);
+    
+    // stop the waveform generation before the next note.
+    noTone(buzzer); 
+  }
+        mRgb.setColor(1,RGB_OFF);
+        mRgb.setColor(2,RGB_OFF);
+        mRgb.show();
+}
+
+void loop() {
+  strip.setPixelColor(i, blue);  //Make Onboard RGB light
+      strip.show();
+  // no need to repeat the melody.
+}
+/**
+* Function       loop
+* @author        Cindy
+* @date          2019.07.29
+* @brief         main fuction
+* @param[in]     void
+* @retval        void
+* @par History   no
+*/
